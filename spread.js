94d60d3c8693d1aaa09d9a3accb59ddb69f84ca4
Masum-Arifin <masumarifinbd@gmail.com>
2022-02-19 22:50:43 +0600

    change file

--- spread.js
+++ spread.js
@@ -1,13 +1,12 @@
 const numbers = [23, 65, 99, 21, 34];
 // console.log(numbers);
-// console.log(...numbers);
+//  console.log(...numbers);
 
 const max = Math.max(56, 42, 78, 86, 97);
 const maxInArray = Math.max(...numbers);
 // console.log(maxInArray);
 
-
 const numbers2 = [...numbers, 88];
 numbers.push(100);
-console.log(numbers);
-console.log(numbers2);
+// console.log(numbers);
+// console.log(numbers2);
